684996981e39d2927662648c51270440970be8e8
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2018-09-21 12:07:20 +0800

    Cluster Evaluvation on outcomes

--- Analyse.R
+++ Analyse.R
@@ -14,3 +14,15 @@ for (i in colnames(X_data)){
 library(cluster)
 X_dist=daisy(X_data,metric="gower")
 h_tree=hclust(X_diss,method = "ward.D2")
+labels=cutree(h_tree,k=2)
+
+#Comparing for the outcomes between the clusters
+y_data$cluster<-labels
+pop1<-subset(y_data,cluster==1)
+pop2<-subset(y_data,cluster==2)
+boxplot(pop1$POCT.CRP,pop2$POCT.CRP)
+t<-kruskal.test(POCT.CRP ~ cluster,data=y_data)
+t
+
+x<-rbind(summary(pop1$VirusYes),summary(pop2$VirusYes))
+fisher.test(x)
