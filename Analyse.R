ace56100a733d81937ae44827848c06a5bd4998b
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2018-10-19 14:40:05 +0800

    Added codes for Cluster Characterization

--- Analyse.R
+++ Analyse.R
@@ -26,3 +26,28 @@ t
 
 x<-rbind(summary(pop1$VirusYes),summary(pop2$VirusYes))
 fisher.test(x)
+
+#Charaterzing the clusters
+X_data$cluster<-labels
+pop1<-subset(X_data,cluster==1)
+pop2<-subset(X_data,cluster==2)
+dif<-list()
+for (i in 1:69){
+  if(class(X_data[,i])=="integer" | class(X_data[,i])=="numeric"){
+    t=wilcox.test(pop1[,i],pop2[,i])
+    #Multiply the p-value value by 2 to account for both side testing
+    dif[[colnames(X_data)[i]]]<-(t$p.value)*2
+  }
+  else{
+    # For categorical data comparision
+    le=levels(factor(X_data[,i]))
+    pop1_r1<-table(factor(pop1[,i],levels=le))
+    pop2_r2<-table(factor(pop2[,i],levels=le))
+    c_t<-rbind(pop1_r1,pop2_r2)
+    f<-fisher.test(c_t,simulate.p.value = TRUE)
+    dif[[colnames(X_data)[i]]]<-(f$p.value)
+  }
+}
+
+#Choosing factors with Alpha value of 0.05
+dif[dif<0.05]
